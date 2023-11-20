a81da6b8a59dc3cfd9af77e57bd7b2cc51cdc9b0
naginooi <xxxkmsz@naver.com>
2023-11-20 17:33:20 +0900

    index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: '/',
+        element: <Main />,
+      },
+      {
+        path: '/book/:id',
+        element: <div>book detail</div>,
+      },
+      {
+        path: '/bestseller',
+        element: <div>bestseller</div>,
+      },
+      {
+        path: '/category/main',
+        element: <div>category list</div>,
+      },
+      {
+        path: '/category/list',
+        element: <div>category list</div>,
+      },
+      {
+        path: '/search',
+        element: <div>search</div>,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
