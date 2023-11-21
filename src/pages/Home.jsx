39c38e0ea4810baeaee1c93353527aeac70efa0b
sooa sung <sungsopal@naver.com>
2023-11-21 18:28:46 +0900

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
-export default function Home(){
-	return(
-		<div>홈!
+export default function Home() {
+	return (
+		<div>홈!!!!!!
 			<Link to="/"><img src="../images/main.jpg" alt="" /></Link>
 		</div>
 
