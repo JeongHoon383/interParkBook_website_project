adb3f3414d8cd5e0a78beadb8ace441d162a237c
naginooi <xxxkmsz@naver.com>
2023-11-22 16:31:26 +0900

    midAdBanner

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainBanner from '../components/Main/Main_MainBanner';
+import Main_Advertise from '../components/Main/Main_Advertise';
 
 export default function Main() {
   return (
     <div className="mainContent" style={{ width: '60%', margin: '30px auto', height: '1500px' }}>
       <MainBanner />
+      <Main_Advertise />
     </div>
   );
 }
