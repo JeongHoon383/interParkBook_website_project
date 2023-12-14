2389b8dfbdfa4ce6c3c68bb648359d6e9666aedf
DESKTOP-CD3PK1M\TJ <rkawk2239@naver.com>
2023-12-14 12:39:00 +0900

    delete some files and css

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer({ children }) {
-  return (
-    <div className="footer" style={{ margin: '50px 0' }}>
-      {children}
-    </div>
-  );
-}
