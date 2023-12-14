0552b494356ed5b0ca87d19fd4453ec2eaa35ce7
DESKTOP-CD3PK1M\TJ <rkawk2239@naver.com>
2023-12-14 14:40:09 +0900

    category_list_page_yun merge develop

--- src/components/Header/CategoryBanner/AllCategory.jsx
+++ src/components/Header/CategoryBanner/AllCategory.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import HeaderCategoryList from './HeaderCategoryList';
-import styled from 'styled-components';
+import React from "react";
+import HeaderCategoryList from "./HeaderCategoryList";
+import styled from "styled-components";
 
 const All = styled.div`
   position: relative;
-  color: #000;
+  color: #666;
   z-index: 99;
   .mainNav {
     z-index: 99;
@@ -12,25 +12,60 @@ const All = styled.div`
     display: flex;
     justify-content: space-between;
     top: 15px;
+    padding: 10px;
     border: 1px solid var(--main);
     background-color: #fff;
+    .category {
+      &:not(:last-child) {
+        border-right: 1px solid #d8d8d8;
+      }
+      &:not(:first-child) {
+        margin-left: 10px;
+      }
+      h3 {
+        margin-bottom: 10px;
+        &:hover{
+          text-decoration: underline;
+        }
+      }
+      .categoryNav {
+        display: flex;
+        font-weight: normal;
+        ul {
+          font-size: 14px;
+          margin-right: 10px;
+          &:not(:first-child){
+            padding-left: 10px;
+            border-left: 1px solid #d8d8d8;
+          }
+          li {
+            width: 120px;
+            margin-bottom: 4px;
+            a {
+              display: inline-block;
+              width: 100%;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              &:hover {
+              color: var(--main);
+              font-weight: bold;
+            }
+            }
+          }
+        }
+      }
+    }
   }
 `;
-
 
 export default function AllCategory() {
   return (
     <All>
       <div className="mainNav">
-        <HeaderCategoryList
-          mall={'국내도서'}
-        />
-        <HeaderCategoryList
-          mall={'외국도서'}
-        />
-        <HeaderCategoryList
-          mall={'전자책'}
-        />
+        <HeaderCategoryList mall={"국내도서"} />
+        <HeaderCategoryList mall={"외국도서"} />
+        <HeaderCategoryList mall={"전자책"} />
       </div>
     </All>
   );
