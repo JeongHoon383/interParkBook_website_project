69273273d5c451e2370bfa9e9f2974dce5c880d1
DESKTOP-CD3PK1M\TJ <rkawk2239@naver.com>
2024-01-16 14:07:03 +0900

    01/16 admin condition

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -111,7 +111,6 @@ const MessageRow = styled(motion.div)`
     }
     span:last-child {
       opacity: 0.8;
- 
     }
   }
 `;
@@ -241,7 +240,8 @@ const Chat = () => {
           layoutId="chatroom"
           onClick={() => setClick((prev) => !prev)}
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 640 512">
+          viewBox="0 0 640 512"
+        >
           <path d="M320 0c17.7 0 32 14.3 32 32V96H472c39.8 0 72 32.2 72 72V440c0 39.8-32.2 72-72 72H168c-39.8 0-72-32.2-72-72V168c0-39.8 32.2-72 72-72H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z" />
         </BotIcon>
       ) : (
@@ -249,12 +249,14 @@ const Chat = () => {
           <TopMenu>
             <motion.span
               initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0, transition: 0.3 }}>
+              animate={{ opacity: 1, y: 0, transition: 0.3 }}
+            >
               4조 고객센터
             </motion.span>
             <span
               className="close_button"
-              onClick={() => setClick((prev) => !prev)}>
+              onClick={() => setClick((prev) => !prev)}
+            >
               <MdClose />
             </span>
           </TopMenu>
@@ -269,7 +271,8 @@ const Chat = () => {
                 type: "tween",
                 ease: "easeIn",
               },
-            }}>
+            }}
+          >
             운영시간 : 10:00 ~ 18:50
           </TimeMenu>
           <ChatArea>
@@ -290,7 +293,8 @@ const Chat = () => {
                       type: "tween",
                       ease: "easeIn",
                     },
-                  }}>
+                  }}
+                >
                   <Chat_Avatar />
                   <div className="message_content">
                     <span className="message_author"> </span>
@@ -317,7 +321,7 @@ const Chat = () => {
                 </MessageRow>
               </li>
               {down.map((v, i) =>
-                v.id == "admin1234" ? (
+                v.id == "admin" ? (
                   <MessageRow>
                     <Chat_Avatar />
                     <div className="message_content">
